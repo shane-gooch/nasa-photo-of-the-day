45e11c7766f683bc975ed96f75c8bfcce7fa9805
shane-gooch <43436342+shane-gooch@users.noreply.github.com>
2019-07-10 15:50:18 -0700

    add functionality for video/pics, added daypicker

--- src/components/APOD.js
+++ src/components/APOD.js
@@ -4,11 +4,13 @@ import ReactPlayer from "react-player";
 
 function APOD() {
   const [nasaAPOD, setNasaAPOD] = useState({});
+  const [date, setDate] = useState("");
+
   useEffect(() => {
     const getNasaAPOD = () => {
       axios
         .get(
-          "https://api.nasa.gov/planetary/apod?api_key=0qNDLQkLyXRtYhOt6OdBJLruhNO0tZFrtNt8giKe"
+          `https://api.nasa.gov/planetary/apod?api_key=0qNDLQkLyXRtYhOt6OdBJLruhNO0tZFrtNt8giKe&date=${date}`
         )
         .then(res => {
           console.log(res);
@@ -18,15 +20,24 @@ function APOD() {
     };
 
     getNasaAPOD();
-  }, []);
+  }, [date]);
+
   return (
     <div className="APODContainer">
       <h1>NASA APOD</h1>
+      <input
+        type="date"
+        min="1995-06-16"
+        onChange={e => setDate(e.target.value)}
+      />
+      <small>{nasaAPOD.date}</small>
+      <h2>{nasaAPOD.title}</h2>
       {nasaAPOD.media_type === "video" ? (
-        <ReactPlayer url={nasaAPOD.url} />
+        <ReactPlayer url={nasaAPOD.url} className="apod" />
       ) : (
-        <img src={nasaAPOD.hdurl} alt="APOD" />
+        <img className="apod" src={nasaAPOD.hdurl} alt="APOD" />
       )}
+      <p className="explanation">{nasaAPOD.explanation}</p>
     </div>
   );
 }
